16bbe766d56dd5ab915d69b7d7b04f894dd53d0a
Logan Cox <mail@logan-cox.com>
2023-05-28 14:53:07 +0100

    POST url endpoint now auto increments and returns the URL and KEY to the consumer

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 	"gorm.io/gorm"
 )
 
+// DB connection that can be used across all route functions
 var DB *gorm.DB
 
+// Instantiate a DB connection via a connection string
 func connectDB(connectionString string) {
 	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 	if err != nil {
@@ -21,22 +23,26 @@ func connectDB(connectionString string) {
 	DB = db
 }
 
+// Model for use with GORM
 type savedURL struct {
-	ID        string `gorm:"primaryKey"`
+	ID        int32 `gorm:"AUTO_INCREMENT;PRIMARY_KEY;not null"`
 	URL       string
 	CreatedAt time.Time
 }
 
+// Representation of JSON expected to be used with POST request to the /api/v1/urlkeys endpont
 type redirectURL struct {
 	URL string `json:"URL"`
 }
 
+// Dummy return to front end for now at /api/v1/urlkeys on GET request
 func readURLKeys(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"redirect": "https//www.google.com",
 	})
 }
 
+// Cteate a key to url mapping at the database level return the key and URL
 func createURLKey(c *gin.Context) {
 	var newRedirectURL redirectURL
 
@@ -48,7 +54,7 @@ func createURLKey(c *gin.Context) {
 	}
 
 	url := savedURL{
-		URL: newRedirectURL.URL, 
+		URL:       newRedirectURL.URL,
 		CreatedAt: time.Now(),
 	}
 
@@ -58,7 +64,10 @@ func createURLKey(c *gin.Context) {
 		panic(result.Error)
 	}
 
-	c.JSON(201, newRedirectURL)
+	c.JSON(201, gin.H{
+		"URL": url.URL,
+		"KEY": url.ID,
+	})
 
 	// Generate a key based on a hash of the url and wrte to DB this means that each URl is unique in the DB and a
 	// URL will always output the same hash
