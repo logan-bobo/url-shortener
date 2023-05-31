624ff11e3d75343350ee69c958b4c182cea5fe9c
Logan Cox <mail@logan-cox.com>
2023-05-31 20:09:50 +0100

    allow PUT to the urlkeys endpoing to update a URL at a given key

--- main.go
+++ main.go
@@ -12,19 +12,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// DB connection that can be used across all route functions
-var DB *gorm.DB
-
-// Instantiate a DB connection via a connection string
-func connectDB(connectionString string) {
-	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	DB = db
-}
-
 // Model for use with GORM
 type savedURL struct {
 	ID        int32  `gorm:"AUTO_INCREMENT;PRIMARY_KEY;not null"`
@@ -35,6 +22,21 @@ type savedURL struct {
 // Representation of JSON expected to be used with POST request to the /api/v1/urlkeys endpont {"URL": "www.example.com"}
 type redirectURL struct {
 	URL string `json:"URL" binding:"required"`
+}
+
+// DB connection that can be used across all route functions
+var DB *gorm.DB
+
+// Instantiate a DB connection via a connection string
+func connectDB(connectionString string) (err error) {
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	DB = db
+
+	return 
 }
 
 // Read the URL to redirect to from a given key
@@ -93,8 +95,8 @@ func createURLKey(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{
-		"URL": newSavedURL.URL,
 		"KEY": newSavedURL.ID,
+		"URL": newSavedURL.URL,
 	})
 }
 
@@ -133,6 +135,58 @@ func deleteURLkey(c *gin.Context) {
 	})
 }
 
+// Update a url that exists at a given key
+func updateURLKey(c *gin.Context) {
+	var updateRedirectURL redirectURL
+
+	// Request cant be bound to struct redirectURL
+	if err := c.BindJSON(&updateRedirectURL); err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid request, please ensure PUT reqeusts to this endpoint match the required JSON structure",
+		})
+
+		return
+	}
+
+	// validate url is valid before we write to database
+	_, err := url.ParseRequestURI(updateRedirectURL.URL)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid request, URL is invalid",
+		})
+
+		return
+	}
+
+	urlID := c.Param("id")
+
+	urlIDI32, err := strconv.ParseInt(urlID, 10, 32)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid request please follow the docuemnted format of /api/v1/urlkeys/1",
+		})
+
+		return
+	}
+
+	var urlInstance = savedURL{
+		ID: int32(urlIDI32),
+		URL: updateRedirectURL.URL,
+		CreatedAt: time.Now(),
+	 }
+
+	result := DB.Save(&urlInstance)
+
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	c.JSON(200, gin.H{
+		"KEY": urlInstance.ID,
+		"URL": urlInstance.URL,
+	})
+}
+
 func main() {
 	// Get main db connection paramaters from host OS
 	dbHost := os.Getenv("DB_HOST")
@@ -152,11 +206,13 @@ func main() {
 	)
 
 	// Connect to the database based on the connection string
-	connectDB(dsn)
+	err := connectDB(dsn)
+	if err != nil {
+		panic(err)
+	}
 
 	// Run automatic datbaase migrations
-	err := DB.AutoMigrate(&savedURL{})
-
+	err = DB.AutoMigrate(&savedURL{})
 	if err != nil {
 		panic(err)
 	}
@@ -172,6 +228,8 @@ func main() {
 
 	// Delete route for url keys
 	r.DELETE("/api/v1/urlkeys/:id", deleteURLkey)
+
+	r.PUT("/api/v1/urlkeys/:id", updateURLKey)
 
 	// Run the application
 	err = r.Run() // listen and serve on 0.0.0.0:8080
